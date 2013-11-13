5bafc5a00462bb242c59e552b326bef5b3be5515
wangxihe <admin@stush.cn>
2013-11-13 08:34:47 -0800

    添加友情连发功能基本实现

--- admin/controls/links.class.php
+++ admin/controls/links.class.php
@@ -14,13 +14,15 @@ class Links extends Common {
 	
 	//处理友情连接添加操作
 	public function doAdd() {
+		//关闭调试功能，因为使用的是dwz返还的是json格式的数据
+		debug(0);
 		//接受并处理表单数据
 		$data = array(
 			'title'=>$_POST['title'],
 			'url'=>$_POST['rul'],
 			'descr'=>$_POST['descr'],
 			'author'=>$_POST['author'],
-			'expire'=>time();
+			'expire'=>time()
 		);
 		
 		//将表单数据插入到数据库总
